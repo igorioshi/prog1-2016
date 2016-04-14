d377af1052a3a28e65457a11c933cabc1619eb1a
igor <ioshi.uni@gmail.com>
2016-04-14 13:29:08 +0000

    soma

---
+++ Soma.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+     int main()
+     
+{
+     
+        int num1, num2;
+        int soma;
+        
+        num1 = 9;
+        num2 = 10;
+        
+        soma = num1 + num2;
+        
+        printf("\nSoma = %d", soma);
+        
+        return 0;
+}
